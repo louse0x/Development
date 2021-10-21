39ee6e83a5025eea1f2819f5749d8704a8753619
louse0x <louse0x@gmail.com>
2021-10-21 11:27:34 +0800

    ""

--- jugo/passive.go
+++ jugo/passive.go
@@ -19,53 +19,47 @@ var (
 
 func init() {
 	// 代理探测
-
 }
 
 func Passive(target string) {
-
 	// Cli Output
-	fmt.Printf("[*] %-30s\n", "Start performing passive information collection...")
-	defer fmt.Printf("[*] %-30s\n", "Passive mode information collection is complete...")
-	fmt.Printf("[*] %-30s: %c[1;4;40;32m%s%c[0m\n", "The purpose of this mission is", 0x1b, target, 0x1b)
+	fmt.Printf("%c[1;0;32m%s%c[0m %-30s\n", 0x1b, "[*]", 0x1b, "Start performing passive information collection...")
+	defer fmt.Printf("%c[1;0;32m%s%c[0m %-30s\n", 0x1b, "[*]", 0x1b, "Passive mode information collection is complete...")
+	fmt.Printf("%c[1;0;32m%s%c[0m %s %c[1;4;40;32m%s%c[0m\n", 0x1b, "[*]", 0x1b, "The purpose of this mission is:", 0x1b, target, 0x1b)
 	apikey_map := map[string]string{
 		// {Apiurl : Toml File Node}
 		"beian": "https://tapi.66sj.cn/api/url_icp?url=",
 	}
 	for key, uri := range apikey_map {
-
+		// Start switch
 		switch key {
 		case "beian":
-			// Start
+			// Start beian
 			url := fmt.Sprintf("%s%s", uri, target)
 			client := resty.New()
 			if resp, err := client.R().SetHeader("Accept", "application/json").Get(url); err == nil {
 				// 正常请求
 				if resp.StatusCode() == 200 {
 					// 正常返回
-
 					if err1 := json.Unmarshal([]byte(resp.Body()), &json_data); err1 != nil {
 						log.Fatalf("unmarshal err = %v\n", err)
-
 					}
-
 					PassiveData.Beian = json_data["data"]
-					fmt.Printf("[+] %-30s %c[1;0;34m%s%c[0m\n", "Beian information obtained successfully", 0x1b, "√", 0x1b)
-
-					fmt.Println(PassiveData.Beian)
+					fmt.Printf("%c[1;0;34m%s%c[0m %-30s\n", 0x1b, "[√]", 0x1b, "Beian information obtained successfully")
+					// fmt.Println(PassiveData.Beian)
 				} else {
 					// 异常返回
 					log.Fatalf("status code error: %d %s", resp.StatusCode(), resp.Status())
-					fmt.Printf("[+] %-30s %c[1;0;31m%s%c[0m\n", "Beian information api interface returns an exception", 0x1b, "x", 0x1b)
-
+					fmt.Printf("%c[1;0;31m%s%c[0m %-30s\n", 0x1b, "[x]", 0x1b, "Beian information api interface returns an exception")
 				}
 			} else {
 				// 异常请求
 				log.Fatal(err)
 			}
-			// End
+			// End beian
 		}
+		// Start switch
 
 	}
-	fmt.Println("Passive")
+	// fmt.Println("Passive")
 }
--- main.go
+++ main.go
@@ -1,30 +1,18 @@
 package main
 
-/*
-
-
- */
-
 import (
 	"fmt"
-	"jugo/jugo"
 	"log"
 	"os"
 	"path/filepath"
-	"sync"
 
 	"github.com/urfave/cli/v2"
 )
 
-var (
-	wg sync.WaitGroup
-)
-
 func init() {
-
 	// Check Dirs
 	wd, _ := os.Getwd()
-	var dirs = [3]string{"raw", "log", "result"}
+	var dirs = [2]string{"log", "result"}
 	for _, value := range dirs {
 		if _, err := os.Stat(filepath.Join(wd, value)); err != nil {
 			// Dir IsNotExist
@@ -33,25 +21,63 @@ func init() {
 			}
 		}
 	}
-
+	// Check config.toml
+	if _, err := os.Stat(filepath.Join(wd, "config.toml")); err != nil {
+		// config.toml IsNotExist
+		if os.IsNotExist(err) {
+			//os.Create("config.toml")
+			file, ferr := os.OpenFile("config.toml", os.O_WRONLY|os.O_CREATE, 0666)
+			if ferr != nil {
+				log.Fatal(ferr)
+			}
+			defer file.Close()
+			// config.toml Content
+			str := "[proxy]\n\n\n[api]\n"
+			file.WriteString(str)
+		}
+	}
 }
 
 func main() {
 	// Cli Start
+	cli_str := `
+    ___  ___  ___  ________  ________     
+   |\  \|\  \|\  \|\   ____\|\   __  \    
+   \ \  \ \  \\\  \ \  \___|\ \  \|\  \   
+ __ \ \  \ \  \\\  \ \  \  __\ \  \\\  \  
+|\  \\_\  \ \  \\\  \ \  \|\  \ \  \\\  \ 
+\ \________\ \_______\ \_______\ \_______\
+ \|________|\|_______|\|_______|\|_______|                              
+			   `
+
 	app := &cli.App{
-		Name:  "boom",
-		Usage: "make an explosive entrance",
+		Name:    "Jugo",
+		Version: "0.1.0dev",
+		Usage:   "An external information gathering toole.",
 		Action: func(c *cli.Context) error {
-			fmt.Println("boom! I say!")
+			fmt.Printf("%s", cli_str)
 			return nil
 		},
 	}
-	if err := app.Run(os.Args); err != nil {
-		log.Fatal(err)
+
+	app.Flags = []cli.Flag{
+		&cli.StringFlag{
+			// target
+			Name:  "t",
+			Value: "www.target.com",
+			Usage: "A given domain name or ip address",
+		},
 	}
 
-	// Passive Collection @jugo/passsive.go
+	cli.HelpFlag = &cli.BoolFlag{
+		Name:  "h",
+		Usage: "Please visit: https://github.com/louse0x/Jugo/help.md",
+	}
 
-	jugo.Passive("www.baidu.com")
+	app.Run(os.Args)
+
+	// Passive Collection @jugo/passsive.go
+	//fmt.Printf("%c[1;0;31m%s%c[0m %-30s\n", 0x1b, "[x]", 0x1b, "Beian information api interface returns an exception")
+	//jugo.Passive("www.baidu.com")
 
 }
